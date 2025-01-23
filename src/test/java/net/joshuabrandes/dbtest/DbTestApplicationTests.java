e106c21b838df3ace3cd76f8f16bae7577fe7e03
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2025-01-23 13:52:51 +0100

    first commit

---
+++ src/test/java/net/joshuabrandes/dbtest/DbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.joshuabrandes.dbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
